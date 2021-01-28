07fb0899acfe22b7de011c306786c33ce79c9d49
scottcm73 <scottcmcmahan@gmail.com>
2021-01-28 13:37:49 -0600

    Changed to newsapi.org

--- api_tut.py
+++ api_tut.py
@@ -1,14 +1,27 @@
 import requests
 import json
 
-quotes = requests.get('https://zenquotes.io/api/random')
-# response = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-print(quotes.status_code)
-response_text=quotes.text
-print(quotes.text)
-quotes_json=quotes.json()
-print(quotes_json)
+# quotes = requests.get('https://zenquotes.io/api/random')
+# # response = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+# print(quotes.status_code)
+# response_text=quotes.text
+# print(quotes.text)
+# quotes_json=quotes.json()
+# print(quotes_json)
 
-for q in quotes_json:
-    print(q['q'])
-    print(q['a'])
+
+
+url = ('http://newsapi.org/v2/everything?'
+       'q=battery&'
+       'from=2021-01-27&'
+       'sortBy=popularity&'
+       'apiKey=d205f0b691144bc0b08de0f03d89b894')
+
+response = requests.get(url)
+
+response_text= response.text
+
+articles_json=response.json()
+
+
+print(articles_json)
