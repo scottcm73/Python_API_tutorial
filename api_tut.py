92ad7bd12a3c165795f8a26b7ed61a6a55a80b3d
Scott McMahan <scottcmcmahan@gmail.com>
2021-01-28 10:25:34 -0600

    Changed api to zenquotes.io/api

--- api_tut.py
+++ api_tut.py
@@ -1,13 +1,14 @@
 import requests
 import json
 
-people = requests.get('http://api.open-notify.org/astros.json')
+quotes = requests.get('https://zenquotes.io/api/random')
 # response = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-print(people.status_code)
-response_text=people.text
-print(people.text)
-people_json=people.json()
-print(people_json)
-print("Number of people in space: ", people_json['number'])
-for p in people_json['people']:
-    print(p['name'])
+print(quotes.status_code)
+response_text=quotes.text
+print(quotes.text)
+quotes_json=quotes.json()
+print(quotes_json)
+
+for q in quotes_json:
+    print(q['q'])
+    print(q['a'])
